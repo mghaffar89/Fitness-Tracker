988823ad31be776a21dd9a26674902c2495a206e
Mina G <mghaffar89@gmail.com>
2020-08-11 21:23:52 -0500

    added api routes

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ _id: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
